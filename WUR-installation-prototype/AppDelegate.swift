48f1c0f00ac4f9b7af06e368152e1678753e3b31
Charlie Williams <c@harl.ie>
2018-05-18 10:36:55 +0100

    Initial Commit

---
+++ WUR-installation-prototype/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  WUR-installation-prototype
+//
+//  Created by Charlie Williams on 18/05/2018.
+//  Copyright © 2018 Charlie Williams. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
